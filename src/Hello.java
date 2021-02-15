7741dee96c2d5c992ba1421febe60fa148c77fd5
Pogácsás Csaba <hummel@gmail.com>
2021-02-15 11:13:31 +0100

    first commit to testing the push to remote repo

---
+++ src/Hello.java
@@ -0,0 +1,30 @@
+public class Hello {
+
+    public static void main(String[] args){
+        /*
+        System.out.println("Hello World");
+
+        int myFirstNumber = 10+5;
+        int mySecondNumber = 12;
+        int myThirdNumber = 6;
+
+        int myTotal= myFirstNumber+ mySecondNumber+myThirdNumber;
+
+        System.out.println(myFirstNumber);
+        System.out.println("myFirstNumber");
+        System.out.println(myTotal);
+
+        int myChallangeNumber = 1000-myTotal;
+        System.out.println(myChallangeNumber);
+        */
+        /*
+        double poundValue=10d;
+        double kgValue=poundValue * 0.453_592_37d;
+        System.out.println(kgValue);
+        */
+
+        
+
+    }
+}
+ 
